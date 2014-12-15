9cf7eaf8ade2f461e2ee0666d99c99c9d2c0e7bb
Yichen Lin <lyc1991417@126.com>
2014-12-15 16:58:30 -0500

    ylin div4 inputPet

--- db_js/database_ylin.js
+++ db_js/database_ylin.js
@@ -2,3 +2,32 @@
  * Created by LinYichen on 12/9/14.
  */
 
+var mysql = require('mariasql');
+
+exports.inputData = function(req, res) {
+    console.log(req.body);
+    console.log(req.files);
+    var connection = mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'1234',
+        database:'CS4241',
+        port:3306
+    });
+
+    //connection.query('INSERT INTO pet_test (petName, petType) VALUES (?, ?)',[req.body.petName,req.body.petType],function  (err,result) {
+    //    if (err) throw err;
+    //    res.send("Created "+JSON.stringify(result));
+    //});
+
+    //connection.query('INSERT INTO pet_info (pet_name, pet_type, pet_color, pet_age, pet_gender, pet_ownerName, pet_ownerLocation, pet_ownerContactInfo) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',[req.body.petName,req.body.petType, req.body.petColor, req.body.petAge, req.body.selection, req.body.ownername, req.body.ownerLocation. req.body.ownerContactInfo],function  (err,result) {
+    //    if (err) throw err;
+    //    res.send("Created "+JSON.stringify(result));
+    //});
+
+    connection.query('INSERT INTO pet_info (pet_name, pet_type, pet_color, pet_age, pet_gender, pet_ownerName, pet_ownerLocation, pet_ownerContactInfo, pet_pictureURL) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',[req.body.petName,req.body.petType, req.body.petColor, req.body.petAge, req.body.selection, req.body.ownerName, req.body.ownerLocation, req.body.ownerContactInfo, req.files.userPhoto.path],function  (err,result) {
+        if (err) throw err;
+        res.send("Created "+JSON.stringify(result));
+    });
+};
+
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@ var fs = require('fs');
 
 var db = require('../db_js/database_yyan');
 var dbyk = require('../db_js/database_ykarita');
+var database_ylin= require('../db_js/database_ylin');
+
+var multer = require('multer');
+var done = false;
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -25,6 +30,24 @@ router.get('/renderJade/:file', function(req, res){
   var file = req.params.file;
   res.render(file);
 });
+
+// Use multer to upload image and generate the new path
+router.use(multer({ dest: './public/images/',
+  rename: function (fieldname, filename) {
+    return filename+Date.now();
+    //return filename;
+  },
+  onFileUploadStart: function (file) {
+    console.log(file.originalname + ' is starting ...')
+  },
+  onFileUploadComplete: function (file) {
+    console.log(file.fieldname + ' uploaded to  ' + file.path)
+    done=true;
+  }
+}));
+
+// Post form data into database including image path
+router.post('/inputData',database_ylin.inputData);
 
 //post handler
 router.post('/query', function(req, res) {
