0ed97bb710b1a67106ff0bf65f0e0057492713dd
YichenLin <lyc1991417@126.com>
2014-12-16 01:00:35 -0500

    Update onLoad.js

--- public/javascripts/onLoad.js
+++ public/javascripts/onLoad.js
@@ -9,18 +9,16 @@ function getContent(fileName){
     xmlHttp.send();
     return xmlHttp.responseText;
 }
-
+// Render Gallery page when button "Gallery" is clicked
 function renderGalleryPage(){
     $.post('/query', [], function(result){
         $('#div4').html(result);
     })
 }
-
+// Render queryPet page when button "I want a pet" is clicked
 function renderQueryPetPage(){
     $('#div4').html(getContent('renderJade/div4_queryPet'));
     queryButtonHandler();
-    //window.addEventListener("load", , false);
-
 }
 
 function renderQueryDogsPage(){
@@ -34,16 +32,16 @@ function renderQueryCatsPage(){
         $('#div4').html(result);
     })
 }
-
+// Render inputPet page when button "I have a pet" is clicked 
 function renderInputPetPage(){
     $('#div4').html(getContent('renderJade/div4_inputPet'));
     submitInputButtonHandler();
 }
-
+// Render inputResult page when user successfully submit the input
 function renderInputResult() {
     $('#div4').html(getContent('renderJade/div4_inputResult'));
 }
-
+// Render statistic page when button "statistic" is clicked, showing 4 graphs using highchart
 function renderStatisticPage(){
     $('#div4').html(getContent('renderJade/div4_statistics'));
 }
@@ -55,7 +53,6 @@ function changeText(id){
     xmlhttp2.onreadystatechange = function () {
         if (xmlhttp2.readyState == 4){
             document.getElementById("instructions" + id).innerHTML = xmlhttp2.responseText;
-
         }
     };
     xmlhttp2.open("GET", "paragraph/" + id, true);
@@ -63,9 +60,9 @@ function changeText(id){
 }
 
 function start() {
-
+    // Load paragraph into div1
     changeText(1);
-
+    
     //add button listeners in div3
     document.getElementById("submit_gallery").addEventListener("click",function(){renderGalleryPage()});
     document.getElementById("submit_queryPet").addEventListener("click",function(){renderQueryPetPage()});
@@ -74,46 +71,8 @@ function start() {
     document.getElementById("submit_queryCats").addEventListener("click",function(){renderQueryCatsPage()});
     document.getElementById("submit_queryDogs").addEventListener("click",function(){renderQueryDogsPage()});
 
-
     //load default div4 page
     renderGalleryPage();
 }
-
-
-//function renderQueryPetPage(){
-//    var xmlhttp = new XMLHttpRequest();
-//
-//    xmlhttp.onreadystatechange = function() {
-//        if(xmlhttp.readyState ==4){
-//            document.getElementById("div4").innerHTML = xmlhttp.responseText;
-//        }
-//    };
-//    xmlhttp.open("GET", "renderJade/renderQueryPet", true);
-//    xmlhttp.send();
-//}
-//
-//function renderInputPetPage(){
-//    var xmlhttp = new XMLHttpRequest();
-//
-//    xmlhttp.onreadystatechange = function() {
-//        if(xmlhttp.readyState ==4){
-//            document.getElementById("div4").innerHTML = xmlhttp.responseText;
-//        }
-//    };
-//    xmlhttp.open("GET", "renderJade/renderInputPet", true);
-//    xmlhttp.send();
-//}
-//
-//function renderStatisticPage(){
-//    var xmlhttp = new XMLHttpRequest();
-//
-//    xmlhttp.onreadystatechange = function() {
-//        if(xmlhttp.readyState ==4){
-//            document.getElementById("div4").innerHTML = xmlhttp.responseText;
-//        }
-//    };
-//    xmlhttp.open("GET", "renderJade/renderStatistic", true);
-//    xmlhttp.send();
-//}
 
 window.addEventListener("load", start, false);
